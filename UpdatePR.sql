253539bfa41f132c776daafd0942d8a8a538ddff
ErickGods <168380550+ErickGods@users.noreply.github.com>
2024-09-19 15:12:43 -0300

    Delete UpdatePR.sql

--- UpdatePR.sql
+++
@@ -1,8 +0,0 @@
-UPDATE dbo_tbl_PartsReturn
-INNER JOIN PartsReturnDownload ON dbo_tbl_PartsReturn.OrderNo = PartsReturnDownload.OrderNo
-SET dbo_tbl_PartsReturn.ReceivedDt = [PartsReturnDownload].[Received Date], 
-    dbo_tbl_PartsReturn.DispatchedDt = [PartsReturnDownload].[Dispatched Date], 
-    dbo_tbl_PartsReturn.PrintedDt = [PartsReturnDownload].[Printed Date], 
-    dbo_tbl_PartsReturn.RequestDt = [PartsReturnDownload].[Request Date], 
-    dbo_tbl_PartsReturn.Status = [PartsReturnDownload].[Status],
-    dbo_tbl_PartsReturn.RONum = [PartsReturnDownload].[Dist]+[PartsReturnDownload].[Dealer]+[PartsReturnDownload].[Claim No];
